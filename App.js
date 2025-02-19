c2d35971dc5ce98efe89648db502736fdc5733ce
voanh <tranvoanh2040@gmail.com>
2025-02-19 14:29:47 +0700

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity,} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      backgroundColor: "#fff",
+    }
+  };
+
+  
+  handleButtonClick = (newColor) =>{
+    this.setState({backgroundColor: newColor});
+  };
+
+  render() {
+    return(
+      <View style = {[styles.container, {backgroundColor: this.state.backgroundColor}]}>
+        <Buttonn
+          backgroundColor = "green"
+          onClick = {this.handleButtonClick}
+          colorChange = "green"
+        />
+        <Buttonn
+          backgroundColor = "blue"
+          onClick = {this.handleButtonClick}
+          colorChange = "blue"
+        />
+        <Buttonn
+          backgroundColor = "brown"
+          onClick = {this.handleButtonClick}
+          colorChange = "brown"
+        />
+        <Buttonn
+          backgroundColor = "yellow"
+          onClick = {this.handleButtonClick}
+          colorChange = "yellow"
+        />
+        <Buttonn
+          backgroundColor = "red"
+          onClick = {this.handleButtonClick}
+          colorChange = "red"
+        />
+        <Buttonn
+
+          backgroundColor = "black"
+          onClick = {this.handleButtonClick}
+          colorChange = "black"
+        />
+      </View>
+    );
+  };
+}
+
+class Buttonn extends Component{
+  render(){
+    const {backgroundColor, onClick, colorChange} = this.props;
+    return(
+      <TouchableOpacity
+        style={[styles.button, { backgroundColor: backgroundColor }]}
+        onPress={() => onClick(colorChange)}
+      >
+        <Text style={styles.buttonText}>{backgroundColor}</Text>
+      </TouchableOpacity>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
+  button: {
+    paddingVertical: 20,
+    paddingHorizontal: 40,
+    borderRadius: 5,
+    marginVertical: 20,
+    width: 250,
+    alignItems: 'center'
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 20,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
-});
+})
+
+export default App;
